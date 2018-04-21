ec37620bf22b274fab7054c6e0e5742fef51e30c
Ankk4 <antero.juutinen@gmail.com>
2018-04-21 20:21:58 +0300

    ei vittu se simuloi

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,42 +11,157 @@ public class Boundary
 public class Player : MonoBehaviour {
 
     [SerializeField]
-    private GameObject m_player;
+    private GameObject player;
     [SerializeField]
-    private Text m_turnTimeText;
+    private Text turnTimeText;
     [SerializeField]
-    private Rigidbody m_rb;
+    private Text inputText;
     [SerializeField]
-    private float m_speed;
+    private Text turnText;
     [SerializeField]
-    private float m_jumpForce;
+    private Rigidbody rb;
     [SerializeField]
-    private float m_turnTime;
+    private float speed;
+    [SerializeField]
+    private float jumpForce;
+    [SerializeField]
+    private float turnTime;
 
-    public Boundary m_boundary;
+    public Boundary boundary;
 
-    private float m_turnTimer;
-    private bool m_turnActive;
+    public enum GameState
+    {
+        menu,
+        action,
+        pause,
+        simulation
+    }
+
+    private GameState currentGameState;
+
+    private float turnTimer;
+    private bool turnActive;
     private Quaternion startinRotation;
-    private bool m_canJump; 
     private Vector3 momentum;
+    private bool canJump;
+    private int recordIterator;
+
+    //private IDictionary<, int> dict = new Dictionary<string, int>();
+    //private Vector3[] velocityRecords;
+    private ArrayList positionRecord;
+
+    private Vector3 startP, endP;
+    private float simulationPointDistance;
+    private int currentSimPoint;
 
     // Use this for initialization
     void Start ()
     {
-        this.m_turnTimer = m_turnTime;
-        this.m_turnActive = true;
-        this.startinRotation = m_rb.rotation;
-        this.m_canJump = true;
+        this.turnTimer = turnTime;
+        this.turnActive = true;
+        this.startinRotation = this.rb.rotation;
+        this.canJump = true;
+        //this.velocityRecords = new Vector3[1000];
+        this.positionRecord = new ArrayList();
+        this.recordIterator = 0;
+        this.currentSimPoint = 0;
+        this.currentGameState = GameState.action;
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (turnActive == false && currentGameState == GameState.action)
+            {
+                currentGameState = GameState.pause;
+                turnText.text = "Pause Turn";
+            }
+            else if (currentGameState == GameState.pause)
+            {
+                currentGameState = GameState.simulation;
+                startP = (Vector3)positionRecord[0];
+                endP = (Vector3)positionRecord[1];
+                turnText.text = "Simulation Turn";
+                ResetForSimulation();
+            }
+        }
 
-	}
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            DebugReset();
+        }
+    }
 
     void FixedUpdate()
+    {
+        if (currentGameState == GameState.action)
+        {
+            HandleActionTurn();
+            turnText.text = "Action Turn";
+        }
+        else if (currentGameState == GameState.pause)
+        {
+
+        }
+        else if (currentGameState == GameState.simulation)
+        {
+            rb.position = Vector3.Lerp(startP, endP, Time.deltaTime);
+            if (currentSimPoint + 1 < positionRecord.Count - 1)
+            {
+                currentSimPoint++;
+                SetNextSimulationPoint(currentSimPoint);
+            }
+        }
+    }
+
+    private void HandleActionTurn()
+    {
+        Move();
+        
+        // record
+        if (turnActive)
+        {
+            //velocityRecords[recordIterator] = rb.velocity;
+            //recordIterator++;
+            positionRecord.Add(rb.position);
+        }
+
+        // Bounding
+        /*rb.position = new Vector3
+        (
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            rb.position.y,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+        );*/
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (canJump)
+            {
+                rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+            }
+        }
+
+        /*if (Input.GetKey(KeyCode.Z) && turnActive == false)
+        {
+            ActionTurnStart();
+        }*/
+
+        if (turnActive)
+        {
+            turnTimer -= Time.deltaTime;
+            turnTimeText.text = turnTimer.ToString("F2");
+            if (turnTimer < 0)
+            {
+                ActionTurnEnd();
+            }
+        }
+    }
+
+    private void Move()
     {
         // Can jump?
         float rayDistance = GetComponent<Collider>().bounds.extents.y + 0.1f;
@@ -56,88 +171,71 @@ public class Player : MonoBehaviour {
 
         if (Physics.Raycast(ray, rayDistance))
         {
-            m_canJump = true;
+            canJump = true;
         }
         else
         {
-            m_canJump = false;
+            canJump = false;
         }
 
         // Movement
-        if (m_canJump)
+        if (canJump)
         {
             float moveHorizontal = Input.GetAxis("Horizontal");
             float moveVertical = Input.GetAxis("Vertical");
             Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-            m_rb.velocity = new Vector3(movement.x * m_speed, m_rb.velocity.y, movement.z * m_speed);
+            rb.velocity = new Vector3(movement.x * speed, rb.velocity.y, movement.z * speed);
         }
         else
         {
-            m_rb.velocity = new Vector3(m_rb.velocity.x, m_rb.velocity.y, m_rb.velocity.z);
-        }
-
-        // Bounding
-        /*m_rb.position = new Vector3
-        (
-            Mathf.Clamp(m_rb.position.x, m_boundary.xMin, m_boundary.xMax),
-            m_rb.position.y,
-            Mathf.Clamp(m_rb.position.z, m_boundary.zMin, m_boundary.zMax)
-        );*/
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (m_canJump)
-            {
-                m_rb.AddForce(new Vector3(0, m_jumpForce, 0), ForceMode.Impulse);
-            }
-        }
-
-        if (Input.GetKey(KeyCode.Z) && m_turnActive == false)
-        {
-            TurnStart();
-        }
-
-        if (Input.GetKey(KeyCode.R))
-        {
-            Reset();
-        }
-
-        if (m_turnActive)
-        {
-            m_turnTimer -= Time.deltaTime;
-            m_turnTimeText.text = m_turnTimer.ToString("F2");
-            if (m_turnTimer < 0)
-            {
-                TurnEnd();
-            }
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, rb.velocity.z);
         }
     }
 
-    void TurnEnd()
+    private void ActionTurnEnd()
     {
-        momentum = m_rb.velocity;
-        m_rb.isKinematic = true;
-        m_turnActive = false;
-        m_turnTimeText.text = "00:00";
+        rb.isKinematic = true;
+        turnActive = false;
+        turnTimeText.text = "00:00";
     }
 
-    void TurnStart()
+    private void ActionTurnStart()
     {
-        m_rb.velocity = momentum;
-        m_rb.isKinematic = false;
-        m_turnActive = true;
-        m_turnTimer = m_turnTime;
+        momentum = rb.velocity;
+        turnTimeText.text = "00:00";
+        rb.isKinematic = false;
+        turnActive = true;
+        turnTimer = turnTime;
     }
 
-    private void Reset()
+    private void DebugReset()
     {
-        m_rb.isKinematic = false;
-        m_turnActive = true;
-        m_turnTimer = m_turnTime;
+        rb.velocity = momentum;
+        rb.isKinematic = false;
+        turnActive = true;
+        turnTimer = turnTime;
 
-        m_rb.position = new Vector3(0f, 2.7f, -7f);
-        m_rb.velocity = Vector3.zero;
-        m_rb.rotation = startinRotation;
+        rb.position = new Vector3(0f, 2.7f, -7f);
+        rb.velocity = Vector3.zero;
+        rb.rotation = startinRotation;
+    }
+
+    private void ResetForSimulation()
+    {
+        rb.isKinematic = false;
+        turnTimer = turnTime;
+        turnTimeText.text = turnTimer.ToString("F2");
+
+        rb.position = new Vector3(0f, 2.7f, -7f);
+        rb.velocity = Vector3.zero;
+        rb.rotation = startinRotation;
+    }
+
+    private void SetNextSimulationPoint(int currentPoint)
+    {
+        startP = (Vector3) positionRecord[currentPoint];
+        endP = (Vector3) positionRecord[currentPoint + 1];
+        simulationPointDistance = Vector3.Distance(startP, endP);
     }
 
     
