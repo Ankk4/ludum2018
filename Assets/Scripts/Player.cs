0498ea31967c403be57c56f2dd3b96650b15e03f
Ankk4 <antero.juutinen@gmail.com>
2018-04-21 12:17:03 +0300

    basic input

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,68 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    [SerializeField]
+    private GameObject m_player;
+    [SerializeField]
+    private Rigidbody m_rb;
+    [SerializeField]
+    private float m_speed;
+    [SerializeField]
+    private float m_jumpForce;
+    private float m_jumpSpeed;
+
+    private bool m_grounded;
+
+    // Use this for initialization
+    void Start ()
+    {
+        this.m_grounded = true;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            m_player.transform.position += Vector3.left * m_speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            m_player.transform.position += Vector3.right * m_speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            m_player.transform.position += Vector3.forward * m_speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            m_player.transform.position += Vector3.back * m_speed * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && m_grounded)
+        {
+            m_rb.velocity = new Vector3(0f, m_jumpForce, 0f);
+            m_grounded = false;
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            Debug.Log("jee");
+            m_grounded = true;
+        }
+    }
 }
